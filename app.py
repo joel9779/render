22e35466f416f0e95ec1c92f8571da03882032bd
joel9779 <jfernandez9779@gmail.com>
2024-11-23 09:57:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
-
-if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
-
 import scipy.stats
 import streamlit as st
 import time
